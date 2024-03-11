e59941fc7db53cf66bfcfaa35d68ca961c1c5e0e
atian155 <atianthony@outlook.com>
2024-03-11 19:40:59 +0200

    Added decimal to binary function

---
+++ dec_to_binary.php
@@ -0,0 +1,17 @@
+<?php
+function decimalToBinary($number = 0)
+{
+  // https://byjus.com/maths/decimal-to-binary/
+  $new = $number;
+  $remainders = [];
+
+  while ($new != 0) {
+    $remainder = $new % 2;
+    array_push($remainders, $remainder);
+    $new = floor($new / 2);
+  }
+
+  $remainders = array_reverse($remainders);
+
+  return implode('', $remainders);
+}
