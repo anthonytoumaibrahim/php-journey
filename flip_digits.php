fafd95be486989075722dc058a6e8f814fc8532c
atian155 <atianthony@outlook.com>
2024-03-11 19:49:02 +0200

    Added flip digits function

---
+++ flip_digits.php
@@ -0,0 +1,14 @@
+<?php
+function flipDigits($number = 0)
+{
+  $number = (string) $number;
+  $result = "";
+  $len = strlen($number);
+
+  // Loop through numbers
+  for ($i = $len - 1; $i >= 0; $i--) {
+    $result .= $number[$i];
+  }
+
+  return (int) $result;
+}
