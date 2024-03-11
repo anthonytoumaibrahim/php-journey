525af4a9000f34592808cf8cfade9fa1e6bd8b13
atian155 <atianthony@outlook.com>
2024-03-12 00:08:47 +0200

    Replaced built-in PHP sort function

--- extract_numbers.php
+++ extract_numbers.php
@@ -2,6 +2,21 @@
 function extractNumbers(string $string = "")
 {
   preg_match_all('/\d+/', $string, $matches);
-  sort($matches[0]);
-  return $matches[0];
+  return sortNumbers($matches[0]);
+}
+
+function sortNumbers(array $numsArray = [])
+{
+  // Bubble sort from Python converted to PHP
+  // https://dev.to/matusstafura/bubble-sort-in-php-846
+  for ($i = count($numsArray) - 1; $i > 0; $i--) {
+    for ($j = 0; $j < $i; $j++) {
+      if ($numsArray[$j] > $numsArray[$j + 1]) {
+        $swapped = $numsArray[$j];
+        $numsArray[$j] = $numsArray[$j + 1];
+        $numsArray[$j + 1] = $swapped;
+      }
+    }
+  }
+  return $numsArray;
 }
